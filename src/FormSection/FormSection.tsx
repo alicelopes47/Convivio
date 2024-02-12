29eca52b93b83350cf388a13f7f9463b58ed3c5c
Alice Lopes <lopesalice4745@gmail.com>
2024-02-12 20:50:53 -0300

    Refactor code to improve performance and readability

--- src/FormSection/FormSection.tsx
+++ src/FormSection/FormSection.tsx
@@ -9,25 +9,33 @@ export const FormSection = () => {
     <div className="form-container">
       <div className="form-content">
         <h1 className="title-content">Faça um orçamento!</h1>
-        <div className="form-fields-container">
-          <input type="text" placeholder="Nome" className="form-field" />
-          <input type="text" placeholder="E-mail" className="form-field" />
-          <input type="text" placeholder="Telefone" className="form-field" />
+        <form
+          action="mailto:brenno1254@gmail.com"
+          className="form-fields-container"
+          method="post"
+        >
+          <input type="text" name="Nome" placeholder="Nome" className="form-field" />
+          <input type="text" name="Email" placeholder="E-mail" className="form-field" />
+          <input type="text" name="telefone" placeholder="Telefone" className="form-field" />
           <input
             type="text"
+            name="Serviço"
             placeholder="Selecione um serviço"
             className="form-field"
           />
-          <input placeholder="Endereço da obra" className="form-field" />
-          <input placeholder="Metragem aprox. (em m²)" className="form-field" />
+          <input placeholder="Endereço da obra" name="address" className="form-field" />
+          <input name="Metros" placeholder="Metragem aprox. (em m²)" className="form-field" />
           <textarea
             rows={5}
             cols={40}
+            name="Mensagem"
             placeholder="Informações Adicionais"
             className="form-field text-area-container"
           />
-          <button className="form-button">Enviar</button>
-        </div>
+          <button type="submit" className="form-button">
+            Enviar
+          </button>
+        </form>
       </div>
       <span className="vr"></span>
       <div className="socials-content">
@@ -37,19 +45,19 @@ export const FormSection = () => {
             <MailOutlineIcon className="social-icon" />
             <p>convivioarquitetura@gmail.com</p>
           </div>
-          <div  className="buceta">
+          <div className="buceta">
             <InstagramIcon className="social-icon" />
             <p>@convivioarquitetura</p>
           </div>
-          <div  className="buceta">
+          <div className="buceta">
             <WhatsAppIcon className="social-icon" />
             <p>Thiago - 6199913244</p>
           </div>
-          <div  className="buceta">
+          <div className="buceta">
             <WhatsAppIcon className="social-icon" />
             <p>Luana - 6199913244</p>
           </div>
-          <div  className="buceta">
+          <div className="buceta">
             <PlaceOutlinedIcon className="social-icon" />
             <p>
               Qi 27, Bloco A, Ed. Guara Shopping, sala 212, Guara II - DF Cep:
