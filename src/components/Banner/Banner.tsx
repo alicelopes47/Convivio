c403f2ec03c656a61b3edb22f769e186ee8af19d
Alice Lopes <lopesalice4745@gmail.com>
2024-02-18 22:35:04 -0300

    Add ts-nocheck and fix styling in Banner and WhoWeAre components

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,13 +1,41 @@
+//@ts-nocheck
+import { useEffect, useRef } from "react";
 import "./Banner.scss";
+
 
 interface Props {
   id: string;
 }
 
 export const Banner = ({ id }: Props) => {
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible' && elementRef.current) {
+        // Salve o nome da animação atual
+        const animationName = window.getComputedStyle(elementRef.current).animationName;
+
+        // Temporariamente remova a animação
+        elementRef.current.style.animationName = 'none';
+
+        // Force um reflow, fazendo o navegador perceber que a animação foi removida
+        void elementRef.current.offsetWidth;
+
+        // Reaplique a animação
+        elementRef.current.style.animationName = animationName;
+      }
+    };
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
   return (
     <>
-      <div className="banner-container" id={id}>
+      <div className="banner-container" ref={elementRef} id={id}>
         <div className="banner-content">
           <div className="banner-title-container">
             <h1>
--- src/components/WhoWeAre/WhoWeAre.tsx
+++ src/components/WhoWeAre/WhoWeAre.tsx
@@ -1,35 +1,36 @@
 import "./WhoWeAre.scss";
 import WhoWeAreImage from "../../assets/whoweare/whoweare.png";
 import ConvivioImage from "../../assets/whoweare/a-convivio.png";
-import ConvivioImageMobile from '../../assets/whoweare/whoweare-mobile.png'
+import ConvivioImageMobile from "../../assets/whoweare/whoweare-mobile.png";
 
 interface Props {
   type: number;
 }
 
-export const WhoWeAre = ({type}: Props) => {
-  return (
-    type === 1 ? (
-      <div className="who-container" id="who-us">
+export const WhoWeAre = ({ type }: Props) => {
+  return type === 1 ? (
+    <div className="who-container" id="who-us">
       <div className="who-content">
         <div className="who-informations layout-padding">
-        <div className="title white-title">Quem somos?</div>
-          <div className="information">
-            <h1>Luana Morais</h1>
-            <p>
-              Arquiteta e pós graduada em gerenciamento de obras. Tem vasta
-              experiência com móveis planejados, projetos de casas de alto
-              padrão e edificações em altura.
-            </p>
-          </div>
-          <div className="information">
-            <h1>Thiago Lopes</h1>
-            <p>
-              Arquiteto e pós graduado em arquitetura hospitalar. Apresenta
-              ampla experiência com iluminação de alto padrão. Destaca-se na
-              concepção de projetos de interiores, sendo especialista em cores e
-              texturas.
-            </p>
+          <div className="title white-title">Quem somos?</div>
+          <div className="informations-container">
+            <div className="information">
+              <h1>Luana Morais</h1>
+              <p>
+                Arquiteta e pós graduada em gerenciamento de obras. Tem vasta
+                experiência com móveis planejados, projetos de casas de alto
+                padrão e edificações em altura.
+              </p>
+            </div>
+            <div className="information">
+              <h1>Thiago Lopes</h1>
+              <p>
+                Arquiteto e pós graduado em arquitetura hospitalar. Apresenta
+                ampla experiência com iluminação de alto padrão. Destaca-se na
+                concepção de projetos de interiores, sendo especialista em cores
+                e texturas.
+              </p>
+            </div>
           </div>
         </div>
         <div className="who-assets">
@@ -37,31 +38,37 @@ export const WhoWeAre = ({type}: Props) => {
         </div>
       </div>
     </div>
-     )
-      : (
-        <div className="who-container" id="who-us">
-        <div className="who-content">
-          <div className="who-informations layout-padding">
+  ) : (
+    <div className="who-container" id="who-us">
+      <div className="who-content">
+        <div className="who-informations layout-padding">
           <div className="title white-title">A Convívio</div>
-            <div className="information">
-              <p>
-              A Convívio é fruto do nosso conviver. Somos um casal de arquitetos apaixonados pelo nosso ofício, e um pelo outro. Então decidimos agregar isso tudo para proporcionar o que temos de melhor a oferecer aos nossos clientes, juntos!
-              </p>
-            </div>
-            <div className="information">
-              <p>
-              A Luana é o lado racional, mais técnica e focada na execução dos projetos. O Thiago mais criativo, se concentra na concepção e em captar às ideias dos clientes. Juntos formamos o melhor time para concretizar os seus sonhos!
-              </p>
-            </div>
+          <div className="informations-container">
+          <div className="information">
+            <p>
+              A Convívio é fruto do nosso conviver. Somos um casal de arquitetos
+              apaixonados pelo nosso ofício, e um pelo outro. Então decidimos
+              agregar isso tudo para proporcionar o que temos de melhor a
+              oferecer aos nossos clientes, juntos!
+            </p>
           </div>
-          <div className="who-assets type-2-desktop">
-            <img alt="imagem convivio" src={ConvivioImage} />
+          <div className="information">
+            <p>
+              A Luana é o lado racional, mais técnica e focada na execução dos
+              projetos. O Thiago mais criativo, se concentra na concepção e em
+              captar às ideias dos clientes. Juntos formamos o melhor time para
+              concretizar os seus sonhos!
+            </p>
           </div>
-          <div className="who-assets type-2-mobile">
-            <img alt="imagem convivio" src={ConvivioImageMobile} />
           </div>
         </div>
+        <div className="who-assets type-2-desktop">
+          <img alt="imagem convivio" src={ConvivioImage} />
+        </div>
+        <div className="who-assets type-2-mobile">
+          <img alt="imagem convivio" src={ConvivioImageMobile} />
+        </div>
       </div>
-      )
+    </div>
   );
 };
