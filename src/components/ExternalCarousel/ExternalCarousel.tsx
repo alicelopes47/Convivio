d25b7b01c0c61168eaad890bf63ad85ccb34e056
Alice Lopes <lopesalice4745@gmail.com>
2024-02-22 19:59:34 -0300

    Refactor ExternalCarousel component styling and add arrow icons

--- src/components/ExternalCarousel/ExternalCarousel.tsx
+++ src/components/ExternalCarousel/ExternalCarousel.tsx
@@ -3,6 +3,8 @@ import './ExternalCarousel.scss'
 import { Carousel } from "react-responsive-carousel"
 import CircleIcon from "@mui/icons-material/Circle";
 import PanoramaFishEyeIcon from "@mui/icons-material/PanoramaFishEye";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 interface Props {
     children?: ReactChild[] | undefined;
@@ -14,7 +16,7 @@ export const ExternalCarousel = ({children, isProject}: Props) => {
         <Carousel
         useKeyboardArrows
         showArrows={false}
-        className="external-carousel-container"
+        className={`carousel ${isProject ? "is-projects" : "is-not-projects"} external-carousel-container`}
         showThumbs={false}
         swipeable={false}
         transitionTime={700}
@@ -25,19 +27,33 @@ export const ExternalCarousel = ({children, isProject}: Props) => {
               {selectedItem ? (
                 <CircleIcon
                   fontSize="medium"
-                  className={`carousel-icon ${isProject ? "is-projects" : ""}`}
+                  className={`carousel-icon`}
                   onClick={clickHandler}
                 />
               ) : (
                 <PanoramaFishEyeIcon
                   fontSize="medium"
-                  className={`carousel-icon ${isProject ? "is-projects" : ""}`}
+                  className={`carousel-icon`}
                   onClick={clickHandler}
                 />
               )}
             </>
           );
         }}
+        renderArrowNext={(clickHandler, hasNext) => (
+          <ArrowForwardIosIcon
+            fontSize="large"
+            className={`carousel-arrow ${isProject ? "" : "is-not-projects"}`}
+            onClick={clickHandler}
+          />
+        )}
+        renderArrowPrev={(clickHandler, hasPrev) => ( 
+          <ArrowBackIosIcon
+            fontSize="large"
+            className={`carousel-arrow prev ${isProject ? "" : "is-not-projects"}`}
+            onClick={clickHandler}  
+          />
+        )}
         >
             {children}
         </Carousel>
